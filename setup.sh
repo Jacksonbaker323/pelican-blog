9743265a846121fbd492de5c630672002b1ced63
Jackson Baker <jackson@jacksonbaker.net>
2019-01-09 21:27:01 -0800

    Updated setup.sh to be fully automatic

--- setup.sh
+++ setup.sh
@@ -4,4 +4,8 @@ virtualenv . &&
 source bin/activate
 pip install -r requirements.txt &&
 git submodule update --init --recursive &&
-pelican-themes -i themes/attila
+pelican-themes -i themes/attila &&
+cd jacksonbaker.net &&
+pelican content &&
+cd output &&
+python -m pelican.server 
